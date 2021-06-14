500dda6bf892b6b379e09b25044ab8ce5ca49e5f
yuan-kaixuan-2002 <83719912+yuan-kaixuan-2002@users.noreply.github.com>
2021-06-15 05:37:17 +0800

    Create Stimmy.py

---
+++ Conditionals/Stimmy.py
@@ -0,0 +1,9 @@
+ans1 = input("Are you over 50?")
+
+ans2 = input("Are you unemployed?")
+ 
+
+if((ans1 == "yes") and (ans2 == "yes")):
+    print("you are qualified!")
+else:
+    print("You are not qualified!")
