435dde49cf1671abb2c34107bbebd2e5745a531a
yuan-kaixuan-2002 <83719912+yuan-kaixuan-2002@users.noreply.github.com>
2021-06-15 05:10:42 +0800

    Update ListDemo.py

--- Lists/ListDemo.py
+++ Lists/ListDemo.py
@@ -1,1 +1,31 @@
-Placeholder
+# Creating lists
+
+lst = []
+
+lst = [1,2,3,4]
+
+# Accessing lists
+
+#first element
+num = lst[0]
+
+#last element
+
+num = lst[-1]
+
+#Useful list functions
+
+#returns the length of lists
+length = len(lst)
+
+#adds element to the back of list
+lst.append(5)
+
+#removes element by index
+lst.pop(1)
+
+#removes element by value
+lst.remove(1)
+
+#returns the index of first occurance of the value
+i = lst.index(1)
