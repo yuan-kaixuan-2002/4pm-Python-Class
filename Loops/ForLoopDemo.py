d2b67816f0018eb17481f5baed39534ab50c5c74
yuan-kaixuan-2002 <83719912+yuan-kaixuan-2002@users.noreply.github.com>
2021-06-15 05:14:31 +0800

    Update ForLoopDemo.py

--- Loops/ForLoopDemo.py
+++ Loops/ForLoopDemo.py
@@ -1,1 +1,24 @@
-placeholder
+#For loop goes through each element of a list, and does something while at it.
+
+nums = [1,2,3,4,5]
+
+#This loop prints out all the elements of nums
+
+for num in nums:
+  print num
+ 
+#This loop sums all the elements in nums
+
+sum = 0
+
+for num in nums:
+  sum+= num
+ print sum
+
+#Use the range() function to iterate through a list of integers, this one prints the integers 0-5
+
+for i in range(6):
+  print(i)
+  
+  
+  
