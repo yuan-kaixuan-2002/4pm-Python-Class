4beb672ae24a055bb6a410bc4a3c09f23bd1a0de
yuan-kaixuan-2002 <83719912+yuan-kaixuan-2002@users.noreply.github.com>
2021-07-03 04:52:59 +0800

    Create TextRunner.py

---
+++ Classes/Gradebook/TextRunner.py
@@ -0,0 +1,31 @@
+from Student import Student
+from Classroom import Classroom
+
+classrooms = []
+
+
+def classTasks(selected):
+    while(True):
+        mode2 = ("Enter 1 to add student, 2 to select students, 3 to quit")
+        if(mode2 == "1"):
+            name = input("Enter a name")
+        break
+
+
+
+
+while(True):
+    mode1 = input("Enter 1 to create class, enter 2 to select class, 3 to quit")
+    print("List of classes:")
+    for c in classrooms:
+        print(c.getName())
+
+    if(mode1 == "1"):
+        temp = input("Enter the name of the class")
+        classrooms.append(Classroom(temp))
+    elif(mode1 == "2"):
+        temp = input("Enter the name of the class")
+        selectedClass = None
+        for c in classrooms:
+            if(c.getName()== temp):
+                classTasks(c)
