eaed371a9ee967de4e8b7018b52e78ed8dae220e
yuan-kaixuan-2002 <83719912+yuan-kaixuan-2002@users.noreply.github.com>
2021-07-06 17:31:07 -0400

    Update TextRunner.py

--- Classes/TicTacToe/TextRunner.py
+++ Classes/TicTacToe/TextRunner.py
@@ -3,3 +3,28 @@ from Board import Board
 board = Board()
 
 board.printBoard()
+
+while(True):
+    while(True):
+        pxr = (int)(input("Choose a row, Player X:"))
+        pxc = (int)(input("Choose a column, Player X:"))
+        if(board.checkMove(pxr,pxc)==True):
+            board.makeMove(pxr,pxc,"x")
+            break
+
+    board.printBoard()
+    if(board.checkWin()==1):
+        print("Player X wins!")
+        break
+
+    while(True):
+        pyr = (int)(input("Choose a row, Player Y:"))
+        pyc = (int)(input("Choose a column, Player Y:"))
+        if(board.checkMove(pyr,pyc)==True):
+            board.makeMove(pyr,pyc,"y")
+            break
+
+    board.printBoard()
+    if(board.checkWin()==1):
+        print("Player Y wins!")
+        break
