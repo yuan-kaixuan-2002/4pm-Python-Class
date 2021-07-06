65564e5e895421fdb0aef9d416a76c977ea1d820
yuan-kaixuan-2002 <83719912+yuan-kaixuan-2002@users.noreply.github.com>
2021-07-06 17:09:02 -0400

    Update Board.py

--- Classes/TicTacToe/Board.py
+++ Classes/TicTacToe/Board.py
@@ -16,6 +16,40 @@ class Board:
         print("c|",b[2][0],"|",b[2][1],"|",b[2][2],"|")
 
 
+
+
     def checkWin(self):
-        b = board
-        if(b.get("a1")==b.get("a2"))
+        b = self.board
+        for n in range(3):
+            if(b[n][0]==b[n][1] ==b[n][2]):
+                if(b[n][0]=="x"):
+                    return 1
+                else:
+                    return 2
+
+            if(b[0][n]==b[1][n]==b[2][n]):
+                if(b[0][n]=="x"):
+                    return 1
+                else:
+                    return 2
+
+        if(b[0][0]==b[1][1] ==b[2][2]):
+            if(b[0][0]=="x"):
+                return 1
+            else:
+                return 2
+
+        if(b[2][0]==b[1][1] ==b[2][0]):
+            if(b[2][0]=="x"):
+                return 1
+            else:
+                return 2
+
+        return 0
+
+
+
+
+
+
+
