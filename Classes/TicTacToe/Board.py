366d87e7a0bea1b0140ee3fd763cc44e600b40ce
yuan-kaixuan-2002 <83719912+yuan-kaixuan-2002@users.noreply.github.com>
2021-07-06 16:57:53 -0400

    Update Board.py

--- Classes/TicTacToe/Board.py
+++ Classes/TicTacToe/Board.py
@@ -1,17 +1,21 @@
 class Board:
     key= [" ","o","x"]
 
-    board = {"a1": " ", "a2" : " ", "a3" : " ",
-             "b1": " ", "b2" : " ", "b3" : " ",
-             "c1": " ", "c2" : " ", "c3" : " "}
+    
+    board = [[" "," "," "],
+             [" "," "," "],
+             [" "," ",""]]
 
     def printBoard(self):
         b = self.board
 
 
         print("   1   2   3")
-        print("a|",b.get("a1"),"|",b.get("a2"),"|",b.get("a3"),"|")
-        print("b|",b.get("b1"),"|",b.get("b2"),"|",b.get("b3"),"|")
-        print("c|",b.get("c1"),"|",b.get("c2"),"|",b.get("c3"),"|")
+        print("a|",b[0][0],"|",b[0][1],"|",b[0][2],"|")
+        print("b|",b[1][0],"|",b[1][1],"|",b[1][2],"|")
+        print("c|",b[2][0],"|",b[2][1],"|",b[2][2],"|")
 
-  
+
+    def checkWin(self):
+        b = board
+        if(b.get("a1")==b.get("a2"))
